5c3105e178faf37e31b7e43196ed6595f8815d48
zeineb <zeineb.benayed@insat.ucar.tn>
2026-05-28 22:17:23 +0200

    Update files and remove vendor/

--- PHP/modify_task/modify_task.php
+++ PHP/modify_task/modify_task.php
@@ -2,8 +2,13 @@
 
 session_start();
 
+<<<<<<< Updated upstream
 require_once '../add-task/db.php';
 require_once '../login_logic/auth_guard.php';
+=======
+require_once __DIR__ . '/../add-task/db.php';
+require_once __DIR__ . '/../login_logic/auth_guard.php';
+>>>>>>> Stashed changes
 
 // Vérifier que l'utilisateur est connecté
 requireLogin();
